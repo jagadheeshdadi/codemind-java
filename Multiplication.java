47d2fec20ba87f01106e2848cfc960f6cf67d569
jagadheeshdadi <120714674+jagadheeshdadi@users.noreply.github.com>
2023-08-10 12:14:37 +0530

    import java.util.Scanner; public class m {     public static void main(String args[])     {         Scanner Narsi = new Scanner(System.in);         int a=Narsi.nextInt();         int b=Narsi.nextInt();         System.out.println(a*b);     } }

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class m
+{
+    public static void main(String args[])
+    {
+        Scanner Narsi = new Scanner(System.in);
+        int a=Narsi.nextInt();
+        int b=Narsi.nextInt();
+        System.out.println(a*b);
+    }
+}
