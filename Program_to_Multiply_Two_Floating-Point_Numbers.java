e2986fa001e92caf5fd4791cca1e21fd1c5df1be
jagadheeshdadi <120714674+jagadheeshdadi@users.noreply.github.com>
2023-08-25 10:04:42 +0530

    import java.util.Scanner; public class siri{     public static void main(String[] args){         float a,b;         Scanner n = new Scanner(System.in);         a=n.nextFloat();         b=n.nextFloat();         System.out.printf("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class siri{
+    public static void main(String[] args){
+        float a,b;
+        Scanner n = new Scanner(System.in);
+        a=n.nextFloat();
+        b=n.nextFloat();
+        System.out.printf("%.2f",a*b);
+    }
+}
